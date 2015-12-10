58a3dbff912fd4d5dc68e34b23d1ad946f82055d
dkmehrmann <dkmehrmann@gmail.com>
2015-12-09 22:19:26 -0600

    finalizing data steps

--- clean_prep.R
+++ clean_prep.R
@@ -175,25 +175,6 @@ all_data <- all_data[-bad.vars]
 
 ######## Exploring values to change to NA #############################
 
-#little function to give missing data information, parameter should be dataframe
-propmiss <- function(dataframe) {
-  m <- sapply(dataframe, function(x) {
-    data.frame(
-      nmiss=sum(is.na(x)), 
-      n=length(x), 
-      propmiss=sum(is.na(x))/length(x)
-    )
-  })
-  d <- data.frame(t(m))
-  d <- sapply(d, unlist)
-  d <- as.data.frame(d)
-  d$variable <- row.names(d)
-  row.names(d) <- NULL
-  d <- cbind(d[ncol(d)],d[-ncol(d)])
-  return(d[order(d$propmiss), ])
-}
-
-
 train_numr = all_data[, sapply(all_data, is.numeric)]
 
 train_numr_samp = train_numr[,sample(1:ncol(train_numr),100)]
